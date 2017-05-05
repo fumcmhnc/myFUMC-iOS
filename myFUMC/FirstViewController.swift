6e88f08badc40e2ae22d65a50f04e2f5ce58d186
Smith Frazier <fsmith@Smiths-MacBook-Pro.local>
2017-05-05 09:53:22 -0400

    Initial Commit

---
+++ myFUMC/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  myFUMC
+//
+//  Created by Smith, Frazier on 5/5/17.
+//  Copyright © 2017 Smith, Frazier. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
